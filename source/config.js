a99acc7a94a960a3e2c20726cdd94c107a3d6908
Zeek Wang <wangzhenhuang.zeek@gmail.com>
2015-11-12 22:19:41 +0800

    config.js

---
+++ source/config.js
@@ -0,0 +1,11 @@
+CONFIG = {
+    url: "http://localhost/mdblog/",
+    title: "Zeek的blog",
+    subtitle: "",
+    description: "",
+    keywords: "BLOG,Zeek Wang,技术",
+    blog: {
+        recentSize: 5,
+        pageSize: 10,
+    }
+}
